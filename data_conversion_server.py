caf6e44688d7710c1103fcd9179019e32d647557
Sandro Pedrett <sandro.pedrett@hotmail.com>
2022-11-13 01:20:38 +0100

    multi processor convertion

--- data_conversion_server.py
+++ data_conversion_server.py
@@ -1,13 +1,28 @@
 import sys
 import os
-
+from multiprocessing import Process
 from data_conversion import convert
 
 
-in_file = sys.argv[1]
-print(f"Process {in_file}")
+OUTPUT_DIR = 'data/out/'
+INPUT_DIR = 'data/'
 
-file_name = os.path.basename(in_file)
-out_path = os.path.join('data/out', file_name)
+if __name__ == '__main__':
+    if not os.path.exists(OUTPUT_DIR):
+        os.mkdir(OUTPUT_DIR)
 
-convert(in_file, out_path)
+    processes = []
+    for i in range(0, 8):
+        print(f"Process {i}")
+        
+        file_name = f"{i}_real_estate_listing.csv"
+        out_path = os.path.join(OUTPUT_DIR, file_name)
+        in_path = os.path.join(INPUT_DIR, file_name)
+        
+        proc = Process(target=convert, args=(in_path, out_path))
+        proc.start()
+        processes.append(proc)
+        
+        
+    for proc in processes:
+        proc.join()
